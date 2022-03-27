3c7a8f4b30f7ed48459dd099863af8f773001c68
Haşim Kılıç <93584838+hasimkilic@users.noreply.github.com>
2022-03-27 23:17:29 +0300

    Add files via upload

---
+++ Exercises/Day-1/script.js
@@ -0,0 +1,78 @@
+//Write a single line comment which says, comments can make code readable
+//Bir tane tek satırlık yoruma şunu yazın: Yorumlar kodları daha okunabilir yapar.
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Bir başka tek satırlı yoruma şunu yazın: Welcome to 30DaysOfJavaScript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+//Çok satırlı başka bir kod yazın :Yorumlar kodları daha okunabilir yapar
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+//variable.js diye dosya oluştur. string boolean undefined ve null tiplerinde veri tipleri oluştur.
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+//birden fazla veri tipini typeof ile kontrol et
+
+// Declare four variables without assigning values
+//4 tane herhangi değer tanılamadan değişken tanımla
+
+// Declare four variables with assigned values
+//4 tane değeri tanımlanmış değişken tanımla
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// tek tek tanımlama first name, last name, marital status, country and age 
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+// tek satırda tanımlama first name, last name, marital status, country and age 
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+// 2 tane değişken tanımla onu console ile yazdır.
+
+
+//!1.soru
+//Yorumlar kodları daha okunabilir yapar
+//!2.soru
+//Welcome to 30DaysOfJavaScript
+//!3.soru
+/*
+Comments can make code readable, easy to reuse and informative
+*/
+//!4.soru ve 5.soru (dosyaları oluşturmadan burada yaptım.)
+let name = 'hasim'; //string
+let yas = 19; //number
+let tik = true; //boolean
+let surname //undefined
+let ınsan = null //null
+console.log(typeof(name))
+console.log(typeof(yas))
+console.log(typeof(tik))
+console.log(typeof(surname))
+console.log(typeof(ınsan));
+//! 6.soru
+'hasim'
+19;
+'Turkey'
+'Trabzon'
+//!7.soru
+let name1 = 'hasim'
+let age = 19
+let country = 'Turkey'
+let city = 'Trabzon';
+//!8.soru
+let firstName = 'Haşim';
+let lastName = 'Kılıç';
+let maritualStatus = 'Bekar';
+let country1 = 'Türkiye';
+let age1 = 19;
+//!9.Soru
+let firstName1 = 'Haşim'
+lastName = 'Kılıç';
+maritualStatus = 'Bekar';
+country1 = 'Türkiye';
+age1 = 19;
+//!10.soru
+const myAge = 19
+const yourAge = 30
+
+console.log('Benim yaşım', myAge)
+console.log('Senin yaşın', yourAge)
